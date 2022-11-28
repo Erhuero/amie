62c5819d21c1d268162d5e060ac9ad525505b3e1
Erhuero <constantin.chtanko@gmail.com>
2022-11-28 14:09:47 +0100

    Test échoué

--- src/test/java/com/mgen/amie/AmieApplicationTests.java
+++ src/test/java/com/mgen/amie/AmieApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mgen.amie;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class AmieApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail("Test échoué poto");
     }
 
 }
